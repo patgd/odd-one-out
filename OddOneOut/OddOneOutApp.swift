f2b00d5a06ef8849ea97ac200665acb730332fad
pat <88806542+patgd@users.noreply.github.com>
2023-01-05 08:39:08 -1000

    Initial Commit

---
+++ OddOneOut/OddOneOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddOneOutApp.swift
+//  OddOneOut
+//
+//  Created by pat on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct OddOneOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
